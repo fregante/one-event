3d88cf918778fa2e602aa6e2e18ab4a56d72a7a5
Federico Brigante <me@fregante.com>
2023-12-21 13:59:27 +0800

    Drop support for boolean-only options

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 export default async function oneEvent(
 	target: EventTarget,
 	type: string,
-	options: boolean | AddEventListenerOptions = {},
+	options: AddEventListenerOptions = {},
 ): Promise<Event> {
-	if (typeof options === 'boolean') {
-		const capture = options;
-		options = {capture};
-	}
-
 	options.once = true;
 	return new Promise(resolve => {
 		target.addEventListener(type, resolve, options);
