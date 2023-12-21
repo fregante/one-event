21c55d5b2f41bf45166404260a64a2364afc8052
Federico Brigante <me@fregante.com>
2023-12-21 23:49:58 +0800

    Resolve with undefined when the AbortSignal is aborted

--- index.ts
+++ index.ts
@@ -2,9 +2,12 @@ export default async function oneEvent(
 	target: EventTarget,
 	type: string,
 	options: AddEventListenerOptions = {},
-): Promise<Event> {
+): Promise<Event | void> {
 	options.once = true;
 	return new Promise(resolve => {
 		target.addEventListener(type, resolve, options);
+		options.signal?.addEventListener('abort', () => {
+			resolve();
+		}, {once: true});
 	});
 }
